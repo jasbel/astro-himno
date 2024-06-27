be0b128ad209323849a325d2454ffb17acaa7ee0
jasbel <asbel.elmec@gmail.com>
2024-06-26 22:03:55 -0400

    [song]

--- src/interfaces/song-interfaces.ts
+++ src/interfaces/song-interfaces.ts
@@ -1,16 +1,20 @@
 type uuidV4 = string;
 
+
+export interface IChoir {
+  id: uuidV4;
+  choir: string;
+};
+export interface IParagraph {
+  id: uuidV4;
+  paragraph: string;
+  chorusPos: [position: number, repeat?: number][];
+};
+
 export interface ISong {
   id: uuidV4;
   title: string;
   musicalNote: "do" | "re" | "mi" | "fa" | "sol" | "la" | "si";
-  chorus: [{
-    id: uuidV4;
-    choir: string;
-  }];
-  paragraphs: [{
-    id: uuidV4;
-    paragraph: string;
-    chorusPos: [position: number, repeat?: number][];
-  }];
+  chorus: IChoir[];
+  paragraphs: IParagraph[];
 }
